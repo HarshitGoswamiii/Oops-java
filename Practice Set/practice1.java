9ae0a355774f99f51892a44f1501b446a16cd194
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-07 21:20:11 +0530

    commit

--- Practice Set/practice1.java
+++ Practice Set/practice1.java
@@ -1,55 +1,76 @@
 import java.util.Scanner;
-//  <------------------------------------------Question 1 ---------------------------------------------------->
-// class Employee {
-// String name;
-//     int salary;
-//     Scanner s = new Scanner(System.in);
 
-//     public void setname() {
-//         System.out.print("Enter Your Name : ");
-//         name = s.nextLine();
-//     }
+class Employee {
+    String name;
+    int salary;
+    Scanner s = new Scanner(System.in);
 
-//     public void getsalary() {
-//         System.out.print("Enter Salary : ");
-//         salary = s.nextInt();
-//     }
+    public void setname() {
+        System.out.print("Enter Your Name : ");
+        name = s.nextLine();
+    }
 
-//     public void getname() {
-//         System.out.println("Name : " + name);
-//         System.out.println("Salary : " + salary);
-//     }
-// }
+    public void getsalary() {
+        System.out.print("Enter Salary : ");
+        salary = s.nextInt();
+    }
 
-// <---------------------------------------------------Question 2------------------------------------------------->
-
-// class cellphone{
-//     public void ringing(){
-//         System.out.println("Ringing.......");
-//     }
-//     public void vibrate(){
-//         System.out.println("Vibraing.......");
-//     }
-//     public void calling(){
-//         System.out.println("Calling.......");
-
-//     }
-// }
-// <---------------------------------------------------Question 3------------------------------------------------->
-public class practice1 {
-    public static void main(String[] args) {
-        // ----------------- Q - 1 ---------------->
-        // Employee name = new Employee();
-        // name.setname();
-        // name.getsalary();
-        // name.getname();
-
-        // ----------------- Q - 2 ---------------->
-        // cellphone phone = new cellphone();
-        // phone.calling();
-        // phone.ringing();
-        // phone.vibrate();
-
-        //  ------------------ Q - 3 -------------->
+    public void getname() {
+        System.out.println("Name : " + name);
+        System.out.println("Salary : " + salary);
     }
 }
+
+// <---------------------------------------------------Question
+// 2------------------------------------------------->
+
+class cellphone {
+    public void ringing() {
+        System.out.println("Ringing.......");
+    }
+
+    public void vibrate() {
+        System.out.println("Vibraing.......");
+
+    }
+
+    public void calling() {
+        System.out.println("Calling.......");
+
+    }
+}
+    // <---------------------------------------------------Question
+    // 3------------------------------------------------->
+    class square {
+        int side;
+
+        public void area() {
+            System.out.println(side * side);
+        }
+
+        public void parameter() {
+            System.out.println(4 * side);
+        }
+    }
+
+    public class practice1 {
+        public static void main(String[] args) {
+            // ----------------- Q - 1 ---------------->
+            // Employee name = new Employee();
+            // name.setname();
+            // name.getsalary();
+            // name.getname();
+
+            // ----------------- Q - 2 ---------------->
+            // cellphone phone = new cellphone();
+            // phone.calling();
+            // phone.ringing();
+            // phone.vibrate();
+
+            // ------------------ Q - 3 -------------->
+            square sq = new square();
+            sq.side = 4;
+            sq.area();
+            sq.parameter();
+        }
+    }
