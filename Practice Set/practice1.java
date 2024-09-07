a31e24c20768616be2c686ba2bb77c3d4ef7e8f1
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-07 20:17:53 +0530

    commit

---
+++ Practice Set/practice1.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class Employee {
+    String name;
+    int salary;
+    Scanner s = new Scanner(System.in);
+
+    public void setname() {
+        System.out.print("Enter Your Name");
+        name = s.nextLine();
+    }
+
+    public void getname() {
+        System.out.println("Name : " + name);
+    }
+
+    public void getsalary() {
+        System.out.print("Enter Salary : ");
+        salary = s.nextInt();
+        System.out.println(salary);
+    }
+}
+
+public class practice1 {
+    public static void main(String[] args) {
+        
+    }
+}
