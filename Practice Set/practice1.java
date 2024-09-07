f8347c5814f99959fef8a8db5fb71500338d22e9
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-07 20:37:32 +0530

    commi

--- Practice Set/practice1.java
+++ Practice Set/practice1.java
@@ -1,31 +1,39 @@
 import java.util.Scanner;
+//  <------------------------------------------Question 1 ---------------------------------------------------->
+// class Employee {
+// String name;
+//     int salary;
+//     Scanner s = new Scanner(System.in);
 
-class Employee {
-    String name;
-    int salary;
-    Scanner s = new Scanner(System.in);
+//     public void setname() {
+//         System.out.print("Enter Your Name : ");
+//         name = s.nextLine();
+//     }
 
-    public void setname() {
-        System.out.print("Enter Your Name : ");
-        name = s.nextLine();
-    }
+//     public void getsalary() {
+//         System.out.print("Enter Salary : ");
+//         salary = s.nextInt();
+//     }
 
-    public void getsalary() {
-        System.out.print("Enter Salary : ");
-        salary = s.nextInt();
-    }
+//     public void getname() {
+//         System.out.println("Name : " + name);
+//         System.out.println("Salary : " + salary);
+//     }
+// }
 
-    public void getname() {
-        System.out.println("Name : " + name);
-        System.out.println("Salary : " + salary);
+// <---------------------------------------------------Question 2------------------------------------------------->
+
+class cellphone{
+    public void ringing(){
+        
     }
 }
-
 public class practice1 {
     public static void main(String[] args) {
-        Employee name = new Employee();
-        name.setname();
-        name.getsalary();
-        name.getname();
+        // ----------------- Q - 1 ---------------->
+        // Employee name = new Employee();
+        // name.setname();
+        // name.getsalary();
+        // name.getname();
     }
 }
