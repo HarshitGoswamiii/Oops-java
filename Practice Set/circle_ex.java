2ef5720e3067a9f8f6e0b1d67d29f1f4d922da01
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-08 18:10:48 +0530

    commit

--- Practice Set/circle_ex.java
+++ Practice Set/circle_ex.java
@@ -5,21 +5,34 @@ class circle {
 
     public void setradius(int radius) {
         r = radius;
+        System.out.println(r);
     }
 
-    public void checkarea(int a) {
+    public void checkarea(float a) {
         area = 3.14f * r * r;
-        if(area==a){
-
+        if (area == a) {
+            System.out.println("Area is Right ! ");
+        } else {
+            System.out.println("Area is Wrong ! ");
         }
     }
 
+    public void checkpara(float p) {
+        para = 2 * 3.14f * r;
+        if (para == p) {
+            System.out.println("Perimeter is Right ! ");
+        } else {
+            System.out.println("Perimeter is Wrong ! ");
+        }
+    }
 }
 
 public class circle_ex {
     public static void main(String[] args) {
         circle clr = new circle();
         clr.setradius(23);
+        clr.checkpara(12.56f);
+        clr.checkarea(12.56f);
 
     }
 }
