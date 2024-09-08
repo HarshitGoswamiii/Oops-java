14a36f6170b48b650dcc54eba4c5c02d251a5a09
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-08 19:42:56 +0530

    commit

---
+++ Practice Set/exer1.java
@@ -0,0 +1,24 @@
+
+import java.util.Random;
+import java.util.Scanner;
+
+class guessnum {
+    int num;
+    int ran;
+
+    public guessnum(int ran) {
+        Random R = new Random();
+        ran = R.nextInt();
+    }
+
+    public int takeuserinput() {
+        Scanner s = new Scanner(System.in);
+
+    }
+}
+
+public class exer1 {
+    public static void main(String[] args) {
+
+    }
+}
