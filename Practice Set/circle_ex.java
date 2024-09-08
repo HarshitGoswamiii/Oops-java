c3e2cbce0ca6fcad4ef5e06dc3cb0a00760d227c
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-08 17:55:17 +0530

    commit

--- Practice Set/circle_ex.java
+++ Practice Set/circle_ex.java
@@ -1,5 +1,27 @@
+class circle {
+    private int r;
+    private int area;
+    private int para;
+
+    public void setradius(int radius) {
+        r = radius;
+    }
+    public void getradius() {
+        System.out.println(r);
+    }
+
+    public void setarea(int a) {
+        area = a;
+    }
+    
+
+    public void setparameter(int p) {
+        para = p;
+    }
+}
+
 public class circle_ex {
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
+
     }
 }
