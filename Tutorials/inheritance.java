b05bac886ab94d019cd8b05d3e3423c2b0e5c916
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-09 20:18:45 +0530

    commit

---
+++ Tutorials/inheritance.java
@@ -0,0 +1,27 @@
+class Animal{
+    public void Speak(){
+        System.out.println("Speaking.......! ");
+    }
+    public void walking(){
+        System.out.println("Walking.......! ");
+    }
+    public void Sniff(){
+        System.out.println("Sniffing.......! ");
+    }
+}
+
+class Dog extends Animal{
+    public void Bark(){
+        System.out.println("Barking.......! ");
+    }
+}
+
+public class inheritance {
+    public static void main(String[] args){
+        Animal A = new Animal();
+        A.Speak();
+
+        Dog dg = new Dog();
+        dg.walking();
+    }
+}
