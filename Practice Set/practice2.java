f60aab252a0c2637de3a1c4a674942de6629d993
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-09 18:41:42 +0530

    commit

---
+++ Practice Set/practice2.java
@@ -0,0 +1,17 @@
+class cylinder {
+    private int radius;
+    private int height;
+
+    public void setradius(int r) {
+        r = radius;
+    }
+    public void getraius(){
+        System.out.println();
+    }
+}
+
+public class practice2 {
+    public static void main(String[] args) {
+
+    }
+}
