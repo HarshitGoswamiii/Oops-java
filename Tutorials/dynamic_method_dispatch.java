7f99530c941094590500c7d48abddf273d2f0c20
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-10 20:38:12 +0530

    phone

--- Tutorials/dynamic_method_dispatch.java
+++ Tutorials/dynamic_method_dispatch.java
@@ -2,10 +2,24 @@ class Phone{
     public void Showtime(){
         System.out.println("The Time Is 18:44 ! ");
     }
+    public void On(){
+        System.out.println("Switching on Phone...... ! ");
+    }
+}
+
+class SmartPhone{
+    public void On(){
+        System.out.println("Switching on Phone...... ! ");
+    }
+    public void music(){
+        System.out.println("Playing Music ! ");
+    }
 }
 
 public class dynamic_method_dispatch {
     public static void main(String[] args){
+
+        
 
     }
 }
