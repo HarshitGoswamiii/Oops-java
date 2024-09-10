a1308aabb2fa14af678d116783ad5869435f359d
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-10 18:43:14 +0530

    commit

---
+++ Tutorials/constructor_inheritance.java
@@ -0,0 +1,19 @@
+class Base{
+    int x;
+    public void Base(){
+        System.out.println("This is Base Class ! ");
+    }
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+}
+public class constructor_inheritance {
+    public static void main(String[] args){
+
+    }
+}
