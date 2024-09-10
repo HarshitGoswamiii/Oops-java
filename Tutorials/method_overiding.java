fab9892aeefca5d04f9c3dc3f9cbc7394ade836f
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-10 19:47:45 +0530

    commti

---
+++ Tutorials/method_overiding.java
@@ -0,0 +1,25 @@
+class A{
+   public void meth1(){
+        System.out.println("This is Method 1 of Class A ! ");
+    }
+    public void meth2(){
+        System.out.println("This is Method 2 of Class A ! ");
+    }
+}
+class B extends A{
+    public void meth12(){
+        System.out.println("This is Method 1 of Class A ! ");
+    }
+    public void meth2(){
+        System.out.println("This is Method 2 of Class B ! ");
+    }
+}
+public class method_overiding {
+    public static void main(String[] args) {
+        A a =new A();
+        a.meth2();
+
+        B b = new B();
+        b.meth2();
+    }
+}
