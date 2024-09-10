20f772659df20b096b367c30ce70fd908220c9a2
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-10 20:36:10 +0530

    commti

---
+++ Tutorials/dynamic_method_dispatch.java
@@ -0,0 +1,11 @@
+class Phone{
+    public void Showtime(){
+        System.out.println("The Time Is 18:44 ! ");
+    }
+}
+
+public class dynamic_method_dispatch {
+    public static void main(String[] args){
+
+    }
+}
