fea05f06506f36b40b0ceef72739a8006e4fe932
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-10 20:57:28 +0530

    commit

--- Practice Set/Practice3.java
+++ Practice Set/Practice3.java
@@ -1,10 +1,23 @@
-class Circle{
+class Circle {
     public int radius;
-    
+
+    public void Area(int r) {
+        // int radius = r;
+        System.out.println("Area : " + 3.14 * r * r);
+    }
 
 }
+
+class Cylinder extends Circle {
+    public int height;
+
+    public void Area(int h) {
+        System.out.println("Area Of Cyinder : " + 2 * 3.14 * r * (r + h));
+    }
+}
+
 public class Practice3 {
     public static void main(String[] args) {
-        
+
     }
 }
