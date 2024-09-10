bbce0bea2c36e1b15dfc37919cdf7e950ab2c5c3
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-10 21:16:44 +0530

    comit

--- Practice Set/Practice3.java
+++ Practice Set/Practice3.java
@@ -1,6 +1,10 @@
 class Circle {
     public int radius;
 
+    public void setRad(int rad) {
+        this.radius = rad;
+    }
+
     public void Area(int r) {
         this.radius = r;
         System.out.println("Area : " + 3.14 * r * r);
@@ -10,16 +14,16 @@ class Circle {
 class Cylinder extends Circle {
     public int height;
 
-    public void Volume(int h) {
-        System.out.println("Volumecommt Of Cyinder : " + 3.14 * radius * radius * h);
+    public void Volume(int h, int rad) {
+        System.out.println("Volume Of Cyinder : " + 3.14 * rad * rad * h);
     }
 }
 
 public class Practice3 {
     public static void main(String[] args) {
-        Circle clr = new Circle();
-        clr.Area(4);
+        // Circle clr = new Circle();
+        // clr.Area(4);
         Cylinder cyl = new Cylinder();
-        cyl.Volume(5);
+        cyl.Volume(4,5);
     }
 }
