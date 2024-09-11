69071aa234ff91f5ae3afc922db1d0ea8a79b558
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-11 18:46:08 +0530

    commit

--- Liabrary.java
+++ Liabrary.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
 class Library{
-    public String[] arr = {"Maths","Scinece","Geography","English","Hindi"};
+    public String[] TotalBook = {"Maths","Scinece","Geography","English","Hindi"};
+    public String[] issuedBook = {};
     public void AddBooks(){
-
+        System.out.print("Enter The Name of Book : ");
+        Scanner B = new Scanner(System.in);
+        String Book = B.nextLine();
     }
     public void IssueBook(){
         
