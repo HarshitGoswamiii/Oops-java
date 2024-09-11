9aad432ca86f974a60768092193c106ba5bebff3
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-11 19:14:37 +0530

    commit

--- Liabrary.java
+++ Liabrary.java
@@ -1,11 +1,15 @@
 import java.util.Scanner;
 class Library{
-    public String[] TotalBook = {"Maths","Scinece","Geography","English","Hindi"};
+    public String[] TotalBook = new String[10];
+    String[] TotalBooks = {"Maths","Science","Geography","English","Hindi"};
     public String[] issuedBook = {};
+    public String[] NewAddedArray = {};
     public void AddBooks(){
         System.out.print("Enter The Name of Book : ");
         Scanner B = new Scanner(System.in);
         String Book = B.nextLine();
+        issuedBook[TotalBooks.length+1] =  Book; 
+        System.out.println(issuedBook);
     }
     public void IssueBook(){
         
@@ -20,6 +24,7 @@ class Library{
 
 public class Liabrary {
     public static void main(String[] args) {
-        
+        Library l = new Library();
+        l.AddBooks();
     }
 }
