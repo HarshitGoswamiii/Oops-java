690a7a5c57fccc443b69b7157f305ae5dee53f40
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-12 19:44:31 +0530

    commit

--- Pattern.java
+++ Pattern.java
@@ -2,20 +2,7 @@ class patterns {
     public void pattern1() {
         int a = 4;
         for (int i = 0; i < a; i++) {
-            for (int b = 1; b <= (a - i - 1); b++) {
-                System.out.print(" ");
-            }
-            for (int j = 0; j <= i; j++) {
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
-    }
-
-    public void pattern3() {
-        int a = 4;
-        for (int i = 0; i < a; i++) {
-            for (int b = (a - i - 1); b > 1; b--) {
+            for (int b = 1; b <= (a-i-1); b++) {
                 System.out.print(" ");
             }
             for (int j = 0; j <= i; j++) {
@@ -49,7 +36,6 @@ public class Pattern {
         patterns p = new patterns();
         // p.pattern2();
         // p.pattern11();
-        // p.pattern1();
-        p.pattern3();
+        p.pattern1();
     }
 }
