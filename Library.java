d7a90726a209200082160ba8a0de802b39340dd2
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-12 17:47:34 +0530

    commit

--- Library.java
+++ Library.java
@@ -1,10 +1,23 @@
-class library{
-   private String AvailbleBook[];
-   private String IssuedBoook[];
-   
-}
-public class Library{
-    public static void main(String[] args){
+class library {
+    String[] Books;
+    int No_of_Books;
 
+    library() {
+        this.Books = new String[10];
+        int No_of_Books = 0;
+    }
+
+    // To Add the Book or Books in Available Library
+    public void AddBook(String Book) {
+        Books[No_of_Books] = Book;
+        No_of_Books++;
+        System.out.println("The Book has Been Added !");
+    }
+}
+
+public class Library {
+    public static void main(String[] args) {
+        library lib = new library();
+        lib.AddBook("Operating System");
     }
 }
