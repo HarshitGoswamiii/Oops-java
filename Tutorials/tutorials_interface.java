81e03942eac7447bdb8e029e10b890910b8b711c
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-19 19:42:08 +0530

    commit

--- Tutorials/tutorials_interface.java
+++ Tutorials/tutorials_interface.java
@@ -1,18 +1,27 @@
-interface Bicycle{
+interface Bicycle {  //interface for Bicycle
     void SpeedUp(int increment);
+
     void ApplyBrake(int decrement);
 }
-interface Car{
+
+interface Car { //interface for Car
     void Suzuki();
+
     void Honda();
 }
 
-class Vehicle implements Bicycle{
+class Vehicle implements Bicycle {
+    int Speed = 10;
+
+    public void SpeedUp(int increment) {
+        Speed = Speed + increment;
+        System.out.println(Speed);
+    }
 
 }
 
 public class tutorials_interface {
     public static void main(String[] args) {
-        
+
     }
 }
