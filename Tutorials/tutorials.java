9da36cfbbe8c1f28d124265075bfd8cd2db5acac
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-19 18:49:09 +0530

    commit

--- Tutorials/tutorials.java
+++ Tutorials/tutorials.java
@@ -1,17 +1,22 @@
 abstract class Shape {
-    abstract public void area();
+    abstract public void area(); // abstract class for calculating area
 
-    abstract public void perimeter();
+    abstract public void perimeter(); // abstract class for calculating perimeter
 
     // abstract public void volume();
 }
 
 class Square extends Shape {
     int a = 3;
+
+    // Calculates Area
+    @Override
     public void area() {
         System.out.println("Area of Square : " + a * a);
     }
 
+    // Calcultes Perimeter
+    @Override
     public void perimeter() {
         System.out.println("Perimeter of Sqaure : " + 4 * a);
     }
@@ -19,6 +24,8 @@ class Square extends Shape {
 
 public class tutorials {
     public static void main(String[] args) {
+        // Shape sh = new Shape(); ---> it throws error bcz it is abstract classs ,So it
+        // cannot be instantiated;
         Square s = new Square();
         s.area();
         s.perimeter();
