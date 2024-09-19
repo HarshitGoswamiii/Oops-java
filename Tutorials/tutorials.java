ffb97f032804f69cda6d457e16a874999bd84de9
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-19 18:37:02 +0530

    commit

--- Tutorials/tutorials.java
+++ Tutorials/tutorials.java
@@ -1,7 +1,23 @@
-class Phone{
-    
+abstract class Shape {
+    abstract public void area();
+
+    abstract public void perimeter();
+
+    abstract public void volume();
 }
+
+class Sqaure extends Shape {
+    public void area(int a) {
+        System.out.println("Area of Square : " + a * a);
+    }
+
+    public void perimeter(int a) {
+        System.out.println("Perimeter of Sqaure : " + 4 * a);
+    }
+}
+
 public class tutorials {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+
     }
 }
