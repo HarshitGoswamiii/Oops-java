5b5d6e2a7b9d5a16e44698620a3781606f87b4e8
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-20 19:43:35 +0530

    update

--- Tutorials/inheritance_interface.java
+++ Tutorials/inheritance_interface.java
@@ -1,15 +1,19 @@
-interface Sample{
+interface Sample {
     public void meth1();
 }
-interface Sample2 extends Sample{
+
+interface Sample2 extends Sample {
     public void meth2();
 }
 
-class MySample implements Sample2{
-
+class MySample implements Sample2 {
+    public void meth2() {
+        System.out.println("Meth 2");
+    }
 }
+
 public class inheritance_interface {
     public static void main(String[] args) {
-        
+
     }
 }
