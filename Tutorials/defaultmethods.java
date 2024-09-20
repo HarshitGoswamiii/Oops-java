006ff740c8a15e43479685239b18569f5511264f
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-20 18:55:21 +0530

    commit

--- Tutorials/defaultmethods.java
+++ Tutorials/defaultmethods.java
@@ -1,13 +1,21 @@
-class CellPhone {
+interface camera {
+    public void TakeSnap();
 
+    public void RecordVideo();
 }
 
-interface Phone {
-    public void GPS();
+interface Wifi {
+    public void wifi(String Network);
+}
 
-    public void MediaPlayer();
+class CellPhone {
+    public void CallNumber(int number) {
+        System.out.println("Calling .... " + number);
+    }
 
-    public void Camera();
+    public void PickCall() {
+        System.out.println("Picking Call.....!");
+    }
 }
 
 public class defaultmethods {
