3e865f47a9c0e7373219371f918f5c1b4bd81530
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-20 19:51:26 +0530

    update

--- Tutorials/inheritance_interface.java
+++ Tutorials/inheritance_interface.java
@@ -2,14 +2,16 @@ interface Sample {
     public void meth1();
 }
 
-interface Sample2 extends Sample {
+interface Sample2 extends Sample {   //inherits methods from the sample interface
     public void meth2();
 }
 
 class MySample implements Sample2 {
+    @Override
     public void meth1() {
         System.out.println("Meth 1");
     }
+    @Override
     public void meth2() {
         System.out.println("Meth 2");
     }
