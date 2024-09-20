93067a9650fbe8c03220d685e6d79af01c5dbee3
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-20 20:11:33 +0530

    update

--- Practice Set/practice4.java
+++ Practice Set/practice4.java
@@ -1,5 +1,14 @@
 // ------------------Q-1-------------------->
 
+abstract class Pen{
+    public void Write(){
+        System.out.println("Writing.....");
+    }
+    public void Refill(){
+        System.out.println("Refilling.......");
+    }
+}
+
 public class practice4 {
     public static void main(String[] args) {
 
