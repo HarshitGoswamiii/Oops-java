69c06bc257d2a7461435e00f09282fe2e1914c92
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-20 20:19:59 +0530

    update

--- Practice Set/practice4.java
+++ Practice Set/practice4.java
@@ -1,10 +1,18 @@
 // ------------------Q-1-------------------->
 
 abstract class Pen {
+    float nib = 0.3f;
+
     abstract public void Write();
-    public void Refill() {
-        System.out.println("Refilling.......");
+
+    abstract public void Refill();
+
+    public void changeNib(float num) {
+        float ch;
+        ch = nib + num;
+        System.out.println("Changing Nib From " + nib + " to " + ch);
     }
+
 }
 
 class Pen1 extends Pen {
@@ -13,10 +21,16 @@ class Pen1 extends Pen {
             System.out.println("Writing.....");
         }
     }
+
+    public void Refill() {
+        System.out.println("Refilling.......");
+    }
 }
 
 public class practice4 {
     public static void main(String[] args) {
+
+        // Q-1
         Pen1 p = new Pen1();
         p.Write();
         p.Refill();
