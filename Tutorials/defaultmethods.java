82c1956822dcfc6f48afeebea8489208c2d1779e
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-20 18:48:35 +0530

    commit

---
+++ Tutorials/defaultmethods.java
@@ -0,0 +1,17 @@
+class CellPhone {
+
+}
+
+interface Phone {
+    public void GPS();
+
+    public void MediaPlayer();
+
+    public void Camera();
+}
+
+public class defaultmethods {
+    public static void main(String[] args) {
+
+    }
+}
