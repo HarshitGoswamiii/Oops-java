9a749d6d34c1b125e911f8950a2675355b72a47c
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-22 22:06:41 +0530

    update

---
+++ calculator.java
@@ -0,0 +1,21 @@
+package calc;
+class Calculator{
+    public void print(){
+        System.out.println("This is Calculator");
+    }
+}
+class ScCalculator{
+    public void print(){
+        System.out.println("This is Sc Calculator");
+    }
+}
+class HybridCalculator{
+    public void print(){
+        System.out.println("This is Hybrid Calculator");
+    }
+}
+public class calculator {
+    public static void main(String[] args) {
+        
+    }
+}
