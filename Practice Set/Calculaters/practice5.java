1020017dc42babeb473d8ea27e0b56bd873d407b
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-22 21:47:34 +0530

    update

---
+++ Practice Set/Calculaters/practice5.java
@@ -0,0 +1,34 @@
+package Calculaters;
+
+class Calculater {
+    int a = 10;
+    int b = 20;
+
+    public void add() {
+        System.out.println(a + b);
+    }
+}
+
+class ScCalculater {
+    int a = 10;
+    int b = 20;
+
+    public void add() {
+        System.out.println(a * b);
+    }
+}
+
+class HybridCalculater {
+    int a = 10;
+    int b = 20;
+
+    public void add() {
+        System.out.println(a - b);
+    }
+}
+
+public class practice5 {
+    public static void main(String[] args) {
+
+    }
+}
--- Practice Set/practice5.java
+++
@@ -1,5 +0,0 @@
-public class practice5 {
-    public static void main(String[] args) {
-
-    }
-}
