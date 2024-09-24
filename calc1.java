98dfcd390daac24a7291524ae30745ad3176383e
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-24 19:57:14 +0530

    update

---
+++ calc1.java
@@ -0,0 +1,14 @@
+class Calulator {
+    int a = 10;
+    int b = 12;
+
+    public void printing() {
+        System.out.println("This is Simple Calculator ! ");
+    }
+}
+
+public class calc1 {
+    public static void main(String[] args) {
+
+    }
+}
