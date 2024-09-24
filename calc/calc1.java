f4c4edbfc99a2d0562b4db439d950dc05d4ab5cf
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-24 20:19:38 +0530

    update

--- calc/calc1.java
+++ calc/calc1.java
@@ -1,3 +1,5 @@
+package calc;
+
 import java.util.Scanner;
 
 class Calculator {
---
+++ pack.java
@@ -0,0 +1,9 @@
+
+import calc.calc1;
+
+public class pack {
+    public static void main(String[] args) {
+        calc1 cg = new calc1();
+        System.out.println(cg.printing);
+    }
+}
