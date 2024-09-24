da7606822d3f01433aba4445eb10b8e5857b72fa
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-24 20:38:07 +0530

    update

--- calc1.java
+++ calc1.java
@@ -1,37 +1,31 @@
-package calc.Calc12;
+
 
 import java.util.Scanner;
 
 class Calculator {
     int a = 10;
     int b = 12;
-
     public void printing() {
         System.out.println("This is Simple Calculator ! ");
         System.out.println(a + b);
     }
 }
-
 class ScCalculator {
     int a = 10;
     int b = 3;
-
     public void printing() {
         System.out.println(a * b);
         System.out.println("This is Sc Calculator ! ");
     }
 }
-
 class HybridCalculator {
     int a = 10;
     int b = 2;
-
     public void printing() {
         System.out.println(a ^ b);
         System.out.println("This is Hybrid Calculator ! ");
     }
 }
-
 public class calc1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
