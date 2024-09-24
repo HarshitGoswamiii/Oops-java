ce48fd9631f04643b275071412db2204c1c76584
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-24 20:00:17 +0530

    update

--- calc1.java
+++ calc1.java
@@ -4,6 +4,27 @@ class Calulator {
 
     public void printing() {
         System.out.println("This is Simple Calculator ! ");
+        System.out.println(a + b);
+    }
+}
+
+class ScCalulator {
+    int a = 10;
+    int b = 12;
+
+    public void printing() {
+        System.out.println(a * b);
+        System.out.println("This is Sc Calculator ! ");
+    }
+}
+
+class HybridCalulator {
+    int a = 10;
+    int b = 12;
+
+    public void printing() {
+        System.out.println(a ^ b);
+        System.out.println("This is Hybrid Calculator ! ");
     }
 }
 
