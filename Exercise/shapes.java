5aa2ff49c4fa601a022dcf8dd2b32d84274d21bf
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-25 19:28:36 +0530

    commit

---
+++ Exercise/shapes.java
@@ -0,0 +1,21 @@
+class Rectangle {
+
+}
+class Circle {
+
+}
+class Square {
+
+}
+class Sphere {
+
+}
+class Cylinder {
+
+}
+
+public class shapes {
+    public static void main(String[] args) {
+
+    }
+}
