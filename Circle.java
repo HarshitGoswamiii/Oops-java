37aade0c6665ae70214552e843ff94e58a6dd463
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-25 20:15:39 +0530

    update

--- Circle.java
+++ Circle.java
@@ -3,6 +3,6 @@ public class Circle extends shapes {
         super(dim1,-1);
     }
     public void Area(){
-        
+        System.out.println((22/4)*this.dim1*this.dim1);
     }
 }
---
+++ sphere.java
@@ -0,0 +1,8 @@
+public class sphere extends shapes {
+    cuboid(int dim1){
+        super(int dim1,-1);
+    }
+    public void Area(int dim1){
+
+    }
+}
