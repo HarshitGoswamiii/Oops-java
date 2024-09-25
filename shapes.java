3d0001dc6ff4d6d7d5eb51664e734b75000f818e
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-25 19:52:51 +0530

    update

---
+++ shapes.java
@@ -0,0 +1,27 @@
+public class shapes {
+    int dim1, dim2;
+
+    shapes(int dim1, int dim2) {
+        this.dim1 = dim1;
+        this.dim2 = dim2;
+    }
+
+    public int GetDim1() {
+        return this.dim1;
+    }
+
+    public int GetDim2() {
+        return this.dim2;
+    }
+
+    public int SetDim1(int dim1) {
+        return this.dim1;
+    }
+
+    public int SetDim2(int dim2) {
+        return this.dim2;
+    }
+
+    public static void main(String[] args) {
+    }
+}
