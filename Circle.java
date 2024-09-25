7fda0d4aee4a577b13037bdbb7f1353cf87259c1
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-25 20:12:09 +0530

    update

---
+++ Circle.java
@@ -0,0 +1,8 @@
+public class Circle extends shapes {
+    Circle(int dim1){
+        super(dim1,-1);
+    }
+    public void Area(){
+        
+    }
+}
---
+++ Rectangle.java
@@ -0,0 +1,11 @@
+public class Rectangle extends shapes {
+    Rectangle(int dim1, int dim2) {
+        super(dim1,dim2);
+    }
+    public void Area(){
+        System.out.println(dim1*dim2);
+    }
+    public static void main(String[] args) {
+        
+    }
+}
