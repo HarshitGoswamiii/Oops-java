fc4462c8da514ae3ff67bbd0f3ec55941eac714b
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-25 20:19:13 +0530

    update

--- sphere.java
+++ sphere.java
@@ -1,8 +1,8 @@
 public class sphere extends shapes {
-    cuboid(int dim1){
-        super(int dim1,-1);
+    sphere(int dim1,int dim2){
+        super(dim1,dim2);
     }
     public void Area(int dim1){
-
+        System.out.println(4*(22/7)*this.dim1*this.dim1);
     }
 }
